b42cef8740a9d331f1624c6f0e3464810c674df9
Cyprien Le Pannérer <cyplp@free.fr>
2015-10-03 16:12:11 +0200

    begin API

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.0'
+
+setup(name='bot',
+      version=version,
+      description="just a twitter bot",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='bot twitter paulla pycon.fr',
+      author='paulla',
+      author_email='',
+      url='',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [console_scripts]
+      bot = bot:main
+      """,
+      )
